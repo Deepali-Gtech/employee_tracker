d8b5d5a7b52a8fe8ecbbf230181620a637fbb6a0
Deepali Garg <deepaligarg.chd@gmail.com>
2021-03-11 21:30:15 -0500

    Added logic to view all employees

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    // Your port, if not 3306
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Be sure to update with your own MySQL password!
+    password: 'password',
+    database: 'employeeTracker_DB',
+  });
+
 function init() {
     inquirer.prompt([
         {
@@ -20,13 +34,26 @@ function init() {
             processChoice(choice, function() {
                 init();
             })
-        }
+        } else {
+            process.exit();
+        } 
+        console.log("I am here. Choice here is --> "+ choice);
     });
 }
 
 function processChoice(choice, callback) {
     console.log("Got choice-> " + choice);
-    callback();
+    if (choice === "View All Employees") {
+        connection.connect((err) => {
+            connection.query("SELECT * FROM employeeTracker_DB.employees", function (err, result, fields) {
+                if (err) throw err;
+                console.table(result);
+                callback();
+              });            
+        })
+    } else {
+        callback();
+    }
 }
 
 init();
